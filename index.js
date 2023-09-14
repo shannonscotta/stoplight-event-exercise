6b4c22f5674b67c33ef11327ecbf553ac7ace8e6
Scott Shannon <scott.a.shannon@outlook.com>
2023-09-14 16:51:14 -0700

    finish part 1

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-(function() {
-  'use strict';
+const stopButton = document.getElementById("stopButton");
+const stopBulb = document.getElementById("stopLight");
 
-  // YOUR CODE HERE
-})();
+
+
+stopButton.addEventListener("click", function (){
+  stopBulb.classList.toggle("stop");
+})
+
+const slowButton = document.getElementById("slowButton");
+const slowBulb = document.getElementById("slowLight");
+
+slowButton.addEventListener("click", function (){
+  slowBulb.classList.toggle("slow")
+})
+
+
+
+const goButton = document.getElementById("goButton");
+const goBulb = document.getElementById("goLight");
+
+goButton.addEventListener("click", function (){
+  goBulb.classList.toggle("go")
+})
